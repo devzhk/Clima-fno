c921ad2acba8aa470c6620c24058100644b5c446
devzhk <devzhk@gmail.com>
2022-11-01 15:00:32 -0700

    add todo list

---
+++ README.md
@@ -0,0 +1,6 @@
+
+TODO: 
+1. Write FNO in terms of real parameters 
+2. EKI class
+3. Training pipeline
+4. Test pipeline
---
+++ models/ensemble.py
@@ -0,0 +1,29 @@
+from turtle import forward
+import torch
+import torch.nn as nn
+
+
+class Ensemble(nn.modules):
+    def __init__(self, num_particles) -> None:
+        super().__init__()
+        pass
+    
+    def init_particles(self, num_particles):
+        '''
+        initialize [num_particles] particles
+        Args:
+            - num_particles: number of particles 
+        '''
+        particles = []
+        return particles
+
+    def forward(self, x) -> None:
+        '''
+        get forward values
+        '''
+        pass
+
+    def update(self) -> None:
+        '''
+        Update parameters for each particle
+        '''
